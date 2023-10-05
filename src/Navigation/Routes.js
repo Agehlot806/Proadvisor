aed3ddb106bfc31383603082127d44d66e932d4e
agehlot <agehlot806@gmail.com>
2023-10-05 18:42:23 +0530

    first commit

---
+++ src/Navigation/Routes.js
@@ -0,0 +1,20 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from '../Screens/Home/HomeScreen';
+
+const Stack = createNativeStackNavigator();
+
+
+const Routes = () => {
+  return (
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} />
+    </Stack.Navigator>
+  </NavigationContainer>
+  )
+}
+
+export default Routes
